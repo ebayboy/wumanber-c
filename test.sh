af7873c16e325c5f14d51a1b682710628272f5f1
fanpengfei1 <fanpengfei@jd.com>
2022-08-08 23:39:35 -0400

    add test.sh

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:test.sh 
+# * @author:ebayboy@163.com 
+# * @date:2022-08-08 23:39 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+./a.out ./testcase/pattern2.txt ./testcase/content2.txt
+
+exit 0
+
